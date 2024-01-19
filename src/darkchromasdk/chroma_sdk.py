9317edbb08a540a62bec4f49b4a26910cf30196b
DarkEden-coding <darkedenc9@gmail.com>
2024-01-19 13:36:23 -0500

    Add docs and clean up prints

--- src/darkchromasdk/chroma_sdk.py
+++ src/darkchromasdk/chroma_sdk.py
@@ -31,11 +31,16 @@ def heartbeat_thread(chromasdk):
 
 
 class ChromaSDK:
-    def __init__(self, config):
-        self.config = config
-        self.map = config['map']
+    def __init__(self, config_path):
+        """
+        Initialize the Chroma SDK
+        :param config_path: the path to the config json file
+        """
+        self.config_path = config_path
+        self.config = json.load(open(config_path))
+        self.map = self.config['map']
 
-        self.id, self.uri = self.initialize()
+        self.id, self.uri = self._initialize()
 
         self.enabled = True
         self.heartbeat_thread = threading.Thread(target=heartbeat_thread, args=(self,))
@@ -43,29 +48,41 @@ class ChromaSDK:
 
         sleep(3)
 
-    def initialize(self):
+    def _initialize(self):
+        """
+        used to initialize the Chroma SDK
+        :return:
+        """
         # send config to server, and get session id
         response = requests.post('http://localhost:54235/razer/chromasdk', json=self.config['chroma'])
         print(f"response from init: {response.json()}")
         try:
             return response.json()['sessionid'], response.json()['uri']
         except KeyError:
-            print("Error initializing Chroma SDK")
-            print(f"response: {response.json()}")
-            exit(1)
+            raise Exception("Error while initializing Chroma SDK")
 
     def save_config(self):
-        json.dump(self.config, open('config.json', 'w'), indent=4)
-
-        print("saved config to config.json")
-        print(f"config: {self.config}")
+        """
+        Save the config to the config file
+        :return:
+        """
+        json.dump(self.config, open(self.config_path, 'w'), indent=4)
 
     def quit(self):
+        """
+        Quit the Chroma SDK
+        :return:
+        """
         requests.delete(self.uri)
         self.enabled = False
         self.heartbeat_thread.join()
 
     def send_grid(self, grid):
+        """
+        Send a grid of color values to the api (6 rows, 22 columns)
+        :param grid: an array of rows of color values (NOT RGB, use parse_rgb in color_parsing.py)
+        :return:
+        """
         data = {
             'effect': 'CHROMA_CUSTOM',
             'param': grid
@@ -77,6 +94,12 @@ class ChromaSDK:
             sleep(2)
 
     def light_index(self, pos, color):
+        """
+        used to only light up one key
+        :param pos: the position in the array (x, y)
+        :param color: the color to light up the key (rgb)
+        :return:
+        """
         parsed_color = parse_rgb(color)
 
         # generate array with 6 rows and 22 columns of 0
@@ -89,6 +112,11 @@ class ChromaSDK:
         self.send_grid(grid)
 
     def light_static(self, color):
+        """
+        light up the entire keyboard with one color
+        :param color: the color to light up the keyboard (rgb)
+        :return:
+        """
         color = parse_rgb(color)
         data = {
             "effect": "CHROMA_STATIC",
@@ -99,6 +127,13 @@ class ChromaSDK:
         requests.put(self.uri + '/keyboard', json=data)
 
     def light_keys(self, keys, color=(0, 0, 0), background=(0, 0, 0)):
+        """
+        light up multiple keys
+        :param keys: a dict of keys and colors or a list of keys
+        :param color: if keys is a list, this is the color to light up the keys (rgb)
+        :param background: the background color (rgb)
+        :return:
+        """
         multi_color = isinstance(keys, dict)
         if multi_color:
             corrected_keys = []
@@ -125,6 +160,10 @@ class ChromaSDK:
         self.send_grid(grid)
 
     def generate_map(self):
+        """
+        used to generate the map of keys to positions
+        :return:
+        """
         self.light_static((0, 0, 0))
 
         saved_keys = []
--- src/darkchromasdk/color_parsing.py
+++ src/darkchromasdk/color_parsing.py
@@ -3,6 +3,11 @@ class DeviceError(Exception):
 
 
 def parse_rgb(color):
+    """
+    parse rgb color to int
+    :param color: color to parse
+    :return:
+    """
     if isinstance(color, (list, tuple)) and len(color) == 3:
         if min(color) >= 0 and max(color) <= 255:
             return (int(color[2]) << 16) | (int(color[1]) << 8) | int(color[0])
@@ -13,6 +18,11 @@ def parse_rgb(color):
 
 
 def parse_hex(color):
+    """
+    parse hex color to int (no #)
+    :param color: color to parse
+    :return:
+    """
     if isinstance(color, str) and 5 < len(color) < 8:
         try:
             return (int(color[-2:], base=16) << 16) | (int(color[-4:-2], base=16) << 8) | int(color[-6:-4], base=16)
